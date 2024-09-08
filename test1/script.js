55dff258834bdd7aa6270175e37cae6af2b317a3
stardestart <99838873+stardestart@users.noreply.github.com>
2024-09-08 14:44:36 +0300

    Update script.js

--- test1/script.js
+++ test1/script.js
@@ -2,7 +2,8 @@ let btnElement = document.querySelector(".btn");
 let resultElement = document.querySelector(".result");
 let radioButtons = document.querySelectorAll('[name="food"]');
 
-btnElement.addEventListener("click", function() {
+btnElement.addEventListener("click", function(event) {
+    event.preventDefault(); // добавьте эту строку
     let total = 0;
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
