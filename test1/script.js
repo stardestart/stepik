5faf384b4bee279340d9b29e8f1d31f91ec5430e
stardestart <99838873+stardestart@users.noreply.github.com>
2024-09-08 15:02:47 +0300

    Update script.js

--- test1/script.js
+++ test1/script.js
@@ -1,14 +1,16 @@
 let btnElement = document.querySelector(".btn");
 let resultElement = document.querySelector(".result");
+let resultContainer = document.querySelector(".result-container");
 let radioButtons = document.querySelectorAll('[name="food"]');
 
 btnElement.addEventListener("click", function(event) {
-    event.preventDefault(); // добавьте эту строку
+    event.preventDefault();
     let total = 0;
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
             total += parseInt(radioButton.value);
         }
     }
-    resultElement.textContent = `Общая стоимость: ${total} руб`;
+    resultElement.textContent = `${total} руб`;
+    resultContainer.style.display = "block"; // show the result container
 });
